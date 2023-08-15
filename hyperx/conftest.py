48776a79374c85b40c7680e807506d487c75e059
smart8099 <abdulbasitm810@gmail.com>
2023-08-15 23:05:35 +0000

    Fix linter and CI issue #4

--- hyperx/conftest.py
+++ hyperx/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from hyperx.users.models import User
-from hyperx.users.tests.factories import UserFactory
+
 
 
 @pytest.fixture(autouse=True)
@@ -9,6 +9,4 @@ def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
 
 
-@pytest.fixture
-def user(db) -> User:
-    return UserFactory()
+
