1bb016a7ada67141d66eed81da0917a54a2dd1b6
smart8099 <abdulbasitm810@gmail.com>
2023-08-23 12:10:49 +0000

    Create custom user model for authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,6 +78,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     # Your stuff: custom apps go here
+    'accounts'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -257,3 +258,4 @@ SPECTACULAR_SETTINGS = {
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
+AUTH_USER_MODEL = 'accounts.User'
---
+++ hyperx/accounts/models.py
@@ -0,0 +1,87 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.hashers import make_password
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, first_name, last_name, email, user_type, password=None):
+        """
+        Create and save a regular user with the given details.
+        """
+        if not first_name:
+            raise ValueError("User should have a first name")
+
+        if not last_name:
+            raise ValueError("User should have a last name")
+
+        if not username:
+            raise ValueError("The username field is required.")
+
+        if not email:
+            raise ValueError("User should have an email")
+
+        if not user_type:
+            raise ValueError("User should have a user type")
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email),
+            user_type=user_type
+        )
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, first_name, last_name, email, user_type, password=None):
+        if password is None:
+            raise ValueError("Password cannot be None")
+
+        if user_type not in ["Manager", "Registrar", "Clinic Supervisor"]:
+            raise ValueError("Invalid user type for creating a superuser")
+
+        user = self.create_user(username,first_name, last_name,  email, user_type, password)
+        user = self.create_user(username, first_name, last_name, email, user_type,password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_CHOICES = (
+        ('Manager', 'Manager'),
+        ('Hospital Supervisor', 'Hospital Supervisor'),
+        ('Registrar', 'Registrar'),
+        ('Nurse', 'Nurse'),
+        ('Lab Technician', 'Lab Technician'),
+        ('Pharmacist', 'Pharmacist'),
+        ('Physician', 'Physician'),
+    )
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255,db_index=True,unique=True)
+    email = models.CharField(max_length=255, unique=True, db_index=True)
+    user_type = models.CharField(max_length=255, choices=USER_TYPE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'user_type']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return f'self.email (self.username)'
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
